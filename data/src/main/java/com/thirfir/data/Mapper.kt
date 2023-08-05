a1ccba1c4b69690495e7bbd59dc461df062e9dbc
Thirfir <sangbbang31@gmail.com>
2023-08-05 20:47:29 +0900

    keyword mapper

--- data/src/main/java/com/thirfir/data/Mapper.kt
+++ data/src/main/java/com/thirfir/data/Mapper.kt
@@ -4,7 +4,8 @@ import com.thirfir.data.datasource.remote.dto.PostDTO
 import com.thirfir.data.datasource.remote.dto.PostHeaderDTO
 import com.thirfir.domain.model.Post
 import com.thirfir.domain.model.PostHeader
-
+import com.thirfir.data.datasource.local.entitiy.KeywordEntity
+import com.thirfir.domain.model.Keyword
 fun PostHeaderDTO.toPostHeader(): PostHeader =
     PostHeader(
         pid = pid,
@@ -19,3 +20,13 @@ fun PostDTO.toPost(): Post =
         body = body,
         file = file
     )
+
+fun KeywordEntity.toKeyword() = Keyword(
+    name = name,
+    createdAt = createdAt,
+)
+
+fun Keyword.toKeywordEntity() = KeywordEntity(
+    name = name,
+    createdAt = createdAt,
+)
