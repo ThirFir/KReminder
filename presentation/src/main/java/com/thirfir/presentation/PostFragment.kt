527bc7fde8af0af77277979c77fe1d0d04bcf573
jangnayoung <skdud4081@gmail.com>
2023-08-10 20:34:57 +0900

    __

--- presentation/src/main/java/com/thirfir/presentation/PostFragment.kt
+++ presentation/src/main/java/com/thirfir/presentation/PostFragment.kt
@@ -27,11 +27,7 @@ class PostFragment : Fragment() {
         else "$this?$query=$number&ls=20&ln=1&dm=r&p=$postnum"
     }
 
-    private val postItems = listOf(
-        PostItem(30976, "일반공지", BASE_URL.addQueryString("b", 14, 30976)),
-        PostItem(30978, "장학공지", BASE_URL.addQueryString("b", 14, 30978)),
-        // 추가 데이터 아이템들...
-    )
+    private val postItems = mutableListOf<PostItem>() // 빈 목록 생성
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,8 +39,21 @@ class PostFragment : Fragment() {
         initRecyclerView()
         initPaginationButtons()
 
+        // 추가 데이터 아이템들...
+        postItems.add(PostItem(30976, "hi", BASE_URL.addQueryString("b", 14, 30976)))
+        postItems.add(PostItem(30978, "hi", BASE_URL.addQueryString("b", 14, 30978)))
+        postItems.add(PostItem(30976, "일반공지", BASE_URL.addQueryString("b", 14, 30976)))
+        postItems.add(PostItem(30978, "장학공지", BASE_URL.addQueryString("b", 14, 30978)))
+        postItems.add(PostItem(30976, "일반공지", BASE_URL.addQueryString("b", 14, 30976)))
+
+        // 추가 데이터 아이템들...
+
+        // 초기에 첫 페이지의 데이터를 표시합니다.
+        updateRecyclerViewData()
+
         return binding.root
     }
+
 
     private fun initClickListeners() {
         postAdapter = PostAdapter { postItem ->
@@ -54,15 +63,30 @@ class PostFragment : Fragment() {
                 .addToBackStack(null)
                 .commit()
         }
+        binding.searchButton.setOnClickListener {
+            val searchText = binding.searchEditText.text.toString()
+            val filteredItems = postItems.filter { it.title.contains(searchText, ignoreCase = true) }
+            updateRecyclerViewData(filteredItems)
+        }
     }
 
     private fun initRecyclerView() {
         binding.recyclerViewPost.layoutManager = LinearLayoutManager(requireContext())
+        postAdapter = PostAdapter { postItem ->
+            val contentFragment = ContentFragment.newInstance(postItem.url)
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.main_fragment_container, contentFragment)
+                .addToBackStack(null)
+                .commit()
+        }
         binding.recyclerViewPost.adapter = postAdapter
-        postAdapter.submitList(postItems)
     }
 
+
     private fun initPaginationButtons() {
+        // 버튼 초기 상태 업데이트
+        updateButtonState()
+
         binding.prevButton.setOnClickListener {
             if (currentPage > 1) {
                 currentPage--
@@ -78,14 +102,46 @@ class PostFragment : Fragment() {
             }
         }
 
-        // 버튼 초기 상태 업데이트
-        updateButtonState()
+        // 각 페이지 버튼에 대한 클릭 리스너 설정
+        binding.page1Button.setOnClickListener {
+            currentPage = 1
+            updateRecyclerViewData()
+        }
+
+        binding.page2Button.setOnClickListener {
+            currentPage = 2
+            updateRecyclerViewData()
+        }
+
+        binding.page3Button.setOnClickListener {
+            currentPage = 3
+            updateRecyclerViewData()
+        }
+
+        binding.page4Button.setOnClickListener {
+            currentPage = 4
+            updateRecyclerViewData()
+        }
+
+        binding.page5Button.setOnClickListener {
+            currentPage = 5
+            updateRecyclerViewData()
+        }
     }
+
 
     private fun updateRecyclerViewData() {
         val start = (currentPage - 1) * postsPerPage
         val end = minOf(start + postsPerPage, postItems.size)
         val sublist = postItems.subList(start, end)
+        postAdapter.submitList(sublist)
+        updateButtonState()
+    }
+
+    private fun updateRecyclerViewData(newList: List<PostItem>) {
+        val start = (currentPage - 1) * postsPerPage
+        val end = minOf(start + postsPerPage, newList.size)
+        val sublist = newList.subList(start, end)
         postAdapter.submitList(sublist)
         updateButtonState()
     }
@@ -99,6 +155,13 @@ class PostFragment : Fragment() {
         binding.page5Button.isEnabled = currentPage != 5
         binding.prevButton.isEnabled = currentPage > 1
         binding.nextButton.isEnabled = currentPage < totalPage
+
+        // 다음 페이지의 버튼들을 업데이트합니다.
+        binding.page1Button.text = (currentPage + 1).toString()
+        binding.page2Button.text = (currentPage + 2).toString()
+        binding.page3Button.text = (currentPage + 3).toString()
+        binding.page4Button.text = (currentPage + 4).toString()
+        binding.page5Button.text = (currentPage + 5).toString()
     }
 
     companion object {
