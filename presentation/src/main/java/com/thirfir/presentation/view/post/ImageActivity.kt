9572424a158bc040af08f1fd73df888a9bdca79d
jangnayoung <skdud4081@gmail.com>
2023-08-23 19:03:43 +0900

    이미지 이동 처리

--- presentation/src/main/java/com/thirfir/presentation/view/post/ImageActivity.kt
+++ presentation/src/main/java/com/thirfir/presentation/view/post/ImageActivity.kt
@@ -2,6 +2,8 @@ package com.thirfir.presentation.view.post
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import com.bumptech.glide.Glide
 import com.thirfir.domain.SRC
@@ -31,11 +33,49 @@ class ImageActivity : AppCompatActivity() {
             .into(binding.imageViewContent)
     }
 
+    private var lastTouchX = 0f
+    private var lastTouchY = 0f
+    private var initialTranslateX = 0f
+    private var initialTranslateY = 0f
     override fun onTouchEvent(event: android.view.MotionEvent): Boolean {
         scaleGestureDetector?.onTouchEvent(event)
+
+        when (event.action) {//이미지 이동 처리 로직
+            MotionEvent.ACTION_DOWN -> {
+                lastTouchX = event.x
+                lastTouchY = event.y
+                initialTranslateX = binding.imageViewContent.translationX
+                initialTranslateY = binding.imageViewContent.translationY
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val deltaX = event.x - lastTouchX
+                val deltaY = event.y - lastTouchY
+
+                binding.imageViewContent.translationX = initialTranslateX + deltaX
+                binding.imageViewContent.translationY = initialTranslateY + deltaY
+            }
+        }
+
+        //    gestureDetector?.onTouchEvent(event)//더블클릭메서드호출
         return true
     }
 
+   /* val gestureDetector = GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
+        override fun onDoubleTap(e: MotionEvent): Boolean { // 더블클릭 이벤트 처리 로직
+            if (scaleFactor > 1.0f) {
+                scaleFactor = 1.0f // 이미지를 원래 크기로 되돌립니다.
+
+            } else {
+                scaleFactor = 2.0f // 이미지를 2배 확대합니다.
+
+            }
+            binding.imageViewContent.scaleX = scaleFactor
+            binding.imageViewContent.scaleY = scaleFactor
+            return true
+        }
+    })
+*/
+   //제스처 이벤트를 처리하는 클래스
     inner class ScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener() {
         override fun onScale(detector: ScaleGestureDetector): Boolean {
             scaleFactor *= detector.scaleFactor
